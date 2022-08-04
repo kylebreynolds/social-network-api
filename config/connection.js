1dfb8094153c1fdbd2138b7836e5b492db5aa4e3
Kyle Reynolds <kbreyn06@gmail.com>
2022-08-04 11:34:39 -0500

    cd

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 
-mongoose.set("debug", true);
+// mongoose.set("debug", true);
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
--- models/Thought.js
+++ models/Thought.js
@@ -64,6 +64,6 @@ ThoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-const Thought = model('Thought', ThoughtSchema) ;
+const Thought = model('Thought', ThoughtSchema);
 
 module.exports = Thought;
